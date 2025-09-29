6302839b69f653b626e607628c31d70ea4598e36
breaxan <abreasandin@gmail.com>
2025-09-29 17:29:23 +0200

    Solved exercise 1!

--- exercise1.py
+++ exercise1.py
@@ -1,1 +1,6 @@
-
+def twoSum(nums, target):
+    for i, val in enumerate(nums):
+        a = nums[:i] + [None] + nums[i+1:]
+        k = target - val
+        if k in a:
+            return i, a.index(k)
